b9334f65d6479043dfe3c540c6e480fc4bcc9954
노니 <ojc123004@gmail.com>
2023-07-05 01:28:32 +0900

    demo

--- Class1.cs
+++ Class1.cs
@@ -1,5 +1,52 @@
-﻿namespace booktokicloring;
+﻿using System.Net;
+using booktokicloring.utils;
 
-public class Class1
+namespace booktokicloring
 {
+    public class Class1
+    {
+        public static void Main()
+        {
+            stringcheak();
+        }
+
+        public static void stringcheak()
+        {
+            string url = "https://newtoki.pro/book/16989/162595";
+            var requested = requestClass.postRequest(url);
+            var htmlparsed = requestClass.dictrequestList(url);
+            foreach (var i in htmlparsed)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void Listcheack()
+        {
+            int cheaknum = 1;
+            string 제목 = "데뷔 못하면 죽을 병 걸림";
+            for (int i = 162595; i <= 162595; i++)
+            {
+                string url = "https://newtoki.pro/book/16989/" + i.ToString();
+
+
+                var requested = requestClass.postRequest(url);
+                List<string> htmlparsed = requestClass.dictrequestList(url);
+                Console.WriteLine(htmlparsed);
+                string filed = @"E:\codingstudy\booktokicloring\output\";
+                string num = cheaknum.ToString();
+                string body = "";
+                htmlparsed.ConvertAll<string>(input =>
+                    {
+                        Console.WriteLine($"[{input}]");
+                        return input;
+                    }
+                );
+
+
+                //System.IO.File.WriteAllText(filed + 제목 + num + ".txt", body);
+                cheaknum++;
+            }
+        }
+    }
 }
--- utils/getData.cs
+++ utils/getData.cs
@@ -1,6 +1,62 @@
-﻿namespace booktokicloring.utils;
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
 
-public class getData
+namespace booktokicloring.utils
 {
-    
+    internal class requestClass
+    {
+        public static string postRequest(string Word)
+        {
+            String callUrl = Word;
+
+            String postData = "a=1&b=2";
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(callUrl);
+            // 인코딩 UTF-8
+            byte[] sendData = UTF8Encoding.UTF8.GetBytes(postData);
+            httpWebRequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+            httpWebRequest.Method = "POST";
+            httpWebRequest.ContentLength = sendData.Length;
+            Stream requestStream = httpWebRequest.GetRequestStream();
+            requestStream.Write(sendData, 0, sendData.Length);
+            requestStream.Close();
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            StreamReader streamReader =
+                new StreamReader(httpWebResponse.GetResponseStream(), Encoding.GetEncoding("UTF-8"));
+            string sRE = streamReader.ReadToEnd();
+
+            streamReader.Close();
+            httpWebResponse.Close();
+            return sRE;
+        }
+
+        public static List<string> dictrequestList(string url)
+        {
+            string html = postRequest(url);
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+            List<string> peno = htmlParserBoldList(htmlDoc);
+            Console.WriteLine("작동됨");
+            return peno;
+        }
+        private static List<string> htmlParserBoldList(HtmlDocument parseHTML)
+        {
+            var XPath = "//*[@id=\"bo_v_con\"]//p[position()>0]";
+            HtmlDocument htmlDoc = parseHTML;
+            string bodyNode = htmlDoc.DocumentNode.SelectSingleNode(XPath)?.InnerText;
+            var mainchild = htmlDoc.DocumentNode.SelectSingleNode(XPath)?.ChildNodes;
+            List<string> array = new List<string>();
+            if (mainchild == null) return null;
+            foreach (var child in mainchild)
+            {
+                array.Add(child.InnerText);
+            }
+
+            return  array;
+        }
+    }
 }
